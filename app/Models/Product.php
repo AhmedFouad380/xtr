87526ec04b5777605dbef39e99fbd1b096073db8
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2023-04-26 22:08:46 +0200

    a

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $appends = ['name','description'];
 
+    public function getNameAttribute()
+    {
+        if (\app()->getLocale() == "ar") {
+            return $this->name_ar;
+        } else {
+            return $this->name_en;
+        }
+    }
+    public function getDescriptionAttribute()
+    {
+        if (\app()->getLocale() == "ar") {
+            return $this->description_ar;
+        } else {
+            return $this->description_en;
+        }
+    }
     public function Category(){
         return $this->belongsTo(Category::class ,'category_id');
     }
