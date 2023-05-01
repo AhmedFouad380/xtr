688257897552b9be88c73a29e8e6b4aff7e741ae
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2023-05-01 19:49:06 +0300

    a

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -67,7 +67,7 @@
                     <a class="nav-link" href="{{url('solutions')}}">{{__('lang.solution')}}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{url('contact-us')}}">{{__('lang.contact-us')}}</a>
+                    <a class="nav-link" href="{{url('contact')}}">{{__('lang.contact-us')}}</a>
                 </li>
             </ul>
             <!-- cart  (mobile)-->
